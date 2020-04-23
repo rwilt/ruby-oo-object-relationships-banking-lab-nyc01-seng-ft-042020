b6db619c6a39c2da6808b768fe0597405d208e75
LogElvis <ningelvis@gmail.com>
2020-04-23 16:03:32 -0400

    completed lab

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,36 @@
-class BankAccount
+# we have two classes (bank account and transfer)
+# we know that the transfer class will process the transaction and check the validity of the bank account class
+# transfer instances can reject transfers depending on status
+# transfer classes start out pending 
+# once transfer is executed it will be changed to complete
+# can be rejected or reversed 
 
+class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
+    def initialize(name)
+        @name = name
+        @balance = 1000 # default, can't change
+        @status = "open"
+    end
+
+    def deposit(amount)
+        @balance += amount
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}." #Elvis = BankAccount.new(Elvis's_account) = --- is ELvis
+    end
+
+    def valid?
+        if self.balance > 0 && self.status == "open"
+            return true
+        else
+            return false
+        end
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,37 @@
 class Transfer
+  attr_reader :sender, :receiver
+  attr_accessor :amount, :status
   # your code here
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+    @transfer_counter = 0
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if valid? && status == "pending" && self.sender.balance > @amount
+      self.sender.balance -= @amount
+      self.receiver.balance += @amount
+      self.status ="complete"
+    else
+      self.status ="rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      self.sender.balance += @amount
+      self.receiver.balance -= @amount
+      self.status = "reversed"
+    end
+
+  end
+
 end
